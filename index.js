5c8e2a105e9875faaddc91b2cc88607d7d62dda6
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-11-30 17:06:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Write your code here!
+// Create a h1 DOM element
 const h1 = document.createElement("h1");
+// Create elements
 var newHeader= h1;
+// Assign an Id to the h1 element
 h1.id = 'victory';
+// Add some text to the lement and all it's children
 h1.textContent =  "is the champion";
